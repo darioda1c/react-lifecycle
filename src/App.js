d58685c03958a233616d7fa56d38ea0323344f22
Dario Daic <dario@daic.me>
2022-09-17 12:36:56 +0200

    Remove everything except react itself

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// function App() {
+//   return (
+//     <div>
+//       <h1>Lets' start!</h1>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    console.log('Constructor')
+    super();
+
+    this.state = {
+      count: 0
+    }
+    this.incrementCount = () => { this.setState({ count: this.state.count + 1 })};
+    this.decrementCount = () => { this.setState({ count: this.state.count - 1 })};
+  }
+
+  componentDidMount() {
+    console.log('Did Mount');
+    console.log('---------');
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Did Update');
+  }
+
+  render() {
+    console.log('Render');
+
+    return(
+      <div>
+        <h1>Hello, there!</h1>
+        <button onClick={this.incrementCount}>Increment</button>
+        <button onClick={this.decrementCount}>Decrement</button>
+      </div>
+    )
+  }
 }
 
 export default App;
