81b7962873f4a20f03f1d4e36c8c63f983097607
Dario Daic <dario@daic.me>
2022-09-17 12:41:12 +0200

    Remove App class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 // function App() {
 //   return (
 //     <div>
@@ -6,39 +5,3 @@ import React from 'react';
 //     </div>
 //   );
 // }
-
-class App extends React.Component {
-  constructor(props) {
-    console.log('Constructor')
-    super();
-
-    this.state = {
-      count: 0
-    }
-    this.incrementCount = () => { this.setState({ count: this.state.count + 1 })};
-    this.decrementCount = () => { this.setState({ count: this.state.count - 1 })};
-  }
-
-  componentDidMount() {
-    console.log('Did Mount');
-    console.log('---------');
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('Did Update');
-  }
-
-  render() {
-    console.log('Render');
-
-    return(
-      <div>
-        <h1>Hello, there!</h1>
-        <button onClick={this.incrementCount}>Increment</button>
-        <button onClick={this.decrementCount}>Decrement</button>
-      </div>
-    )
-  }
-}
-
-export default App;
